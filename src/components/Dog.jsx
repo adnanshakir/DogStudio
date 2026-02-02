5e3254782c8ece3045f66353105084a3d78d9806
Adnan <adnanshakir780@gmail.com>
2026-02-02 22:45:29 +0530

    added texture in branches

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,7 +1,13 @@
 import * as THREE from "three";
-import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
+import {
+  OrbitControls,
+  useGLTF,
+  useTexture,
+  useAnimations,
+} from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
-import { sample } from "three/tsl";
+import { sample, texture } from "three/tsl";
+import { useEffect } from "react";
 
 const Dog = () => {
   const model = useGLTF("/models/dog.drc.glb");
@@ -12,34 +18,49 @@ const Dog = () => {
     gl.outputColorSpace = THREE.SRGBColorSpace;
   });
 
-  const textures = useTexture({
-    normalMap:"models/dog_normals.jpg",
-    sampleMatCap:"/matcap/mat-2.png"
-  })
+  const { actions } = useAnimations(model.animations, model.scene);
 
-  textures.normalMap.flipY = false
-  textures.sampleMatCap.colorSpace = THREE.SRGBColorSpace
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, [actions]);
+
+  const [normalMap, sampleMatCap, branchMap, branchNormalMap] = useTexture([
+    "models/dog_normals.jpg",
+    "/matcap/mat-1.png",
+    "/models/branches_diffuse.jpg",
+    "/models/branches_normals.jpg",
+  ]).map((texture) => {
+    texture.flipY = false;
+    texture.colorSpace = THREE.SRGBColorSpace;
+    return texture;
+  });
+
+  const dogMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: normalMap,
+    matcap: sampleMatCap,
+  });
+
+  const brachMaterial = new THREE.MeshMatcapMaterial({
+    normalMap: normalMap,
+    map: branchMap,
+  });
 
   model.scene.traverse((child) => {
-    if(child.name.includes("DOG")){
-      child.material = new THREE.MeshMatcapMaterial({
-        normalMap:textures.normalMap,
-        matcap:textures.sampleMatCap
-      })
-    }
-  })
-
+    if (child.name.includes("DOG")) {
+      child.material = dogMaterial;
+    } else child.material = brachMaterial;
+  });
 
   return (
     <>
       <primitive
         object={model.scene}
-        position={[.2, -0.72, .1]}
-        rotation={[0, Math.PI/4.8, 0]}
+        position={[0.2, -0.72, 0.1]}
+        rotation={[0, Math.PI / 4.8, 0]}
         scale={1.2}
       />
       <directionalLight position={[0, 5, 5]} color={0xfff} intensity={10} />
-      <OrbitControls />
+      {/* <OrbitControls /> */}
     </>
   );
 };
