859449d9bf24588465f07efa8332ea0a8ba2c8fd
Adnan <adnanshakir780@gmail.com>
2026-02-02 20:53:44 +0530

    Fixed texture & color of the model

--- src/components/Dog.jsx
+++ src/components/Dog.jsx
@@ -1,14 +1,34 @@
-import { OrbitControls, useGLTF } from "@react-three/drei";
-import React from "react";
+import * as THREE from "three";
+import { OrbitControls, useGLTF, useTexture } from "@react-three/drei";
 import { useThree } from "@react-three/fiber";
+import { sample } from "three/tsl";
 
 const Dog = () => {
   const model = useGLTF("/models/dog.drc.glb");
 
   useThree(({ camera, gl, scene }) => {
     camera.position.z = 0.6;
-    console.log(camera.position);
+    gl.toneMapping = THREE.ReinhardToneMapping;
+    gl.outputColorSpace = THREE.SRGBColorSpace;
   });
+
+  const textures = useTexture({
+    normalMap:"models/dog_normals.jpg",
+    sampleMatCap:"/matcap/mat-2.png"
+  })
+
+  textures.normalMap.flipY = false
+  textures.sampleMatCap.colorSpace = THREE.SRGBColorSpace
+
+  model.scene.traverse((child) => {
+    if(child.name.includes("DOG")){
+      child.material = new THREE.MeshMatcapMaterial({
+        normalMap:textures.normalMap,
+        matcap:textures.sampleMatCap
+      })
+    }
+  })
+
 
   return (
     <>
